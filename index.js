501f854c76a7ae7b6a794599d2df2e5922f4044d
Chris Philbin <chris@chrisphilbin.net>
2019-06-22 08:59:10 -0400

    Done.

--- index.js
+++ index.js
@@ -17,3 +17,9 @@ let driversByRevenue = function(drivers) {
 		return d1.revenue - d2.revenue
 	});
 };
+
+let driversByName = function(drivers) {
+	return drivers.slice().sort(function(d1, d2){
+		return d1.name.prototype.localeCompare(d2.name)
+	});
+};
