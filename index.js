a50cc7f0e74e534328d77c0ad30781d128e3c7c2
Chris Philbin <chris@chrisphilbin.net>
2019-06-22 09:00:41 -0400

    Done.

--- index.js
+++ index.js
@@ -20,6 +20,16 @@ let driversByRevenue = function(drivers) {
 
 let driversByName = function(drivers) {
 	return drivers.slice().sort(function(d1, d2){
-		return d1.name.prototype.localeCompare(d2.name)
+		return d1.name.localeCompare(d2.name)
 	});
+};
+
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
+
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
 };
